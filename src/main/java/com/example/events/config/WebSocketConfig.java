b882db5ffdb4ba54fb4f5269e591547c3318e568
rania gasmi <rania.gasmi@esprit.tn>
2025-04-20 13:49:57 +0100

    WebSocketConfig and SecurityConfig

--- src/main/java/com/example/events/config/WebSocketConfig.java
+++ src/main/java/com/example/events/config/WebSocketConfig.java
@@ -11,15 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // Enables a simple in-memory broker
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("http://localhost:4200")
+            .withSockJS();  // Add SockJS fallback
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
-                .withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
 }
